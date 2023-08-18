74bc4f1f243309715bf46286021ddb712a854877
badinkajink <williamxie00@gmail.com>
2023-08-17 23:53:09 -0400

    webdev ghell

--- codex_robotpy/demo-site/server.py
+++ codex_robotpy/demo-site/server.py
@@ -1,50 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import openai
 import os
 
 app = Flask(__name__)
 
 # Set your OpenAI API key here
-openai.api_key_path = "api_key.txt"
+openai.api_key_path = "openai_api_key.txt"
+generate_clicked = False
+prompt = "# Enter your prompt here or upload a file"
+completion = ""
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    prompt = ""
-    completion = ""
-
-    if request.method == "POST":
-        prompt = request.form.get("prompt")
-        print(prompt)
-        print(type(request.form.get("generate")))
-        # Only make the OpenAI API request if the "Generate" button is clicked
-        if request.form.get("generate"):
-            print("receive generate")
-            # Use the OpenAI API to generate a completion using "codex" engine
-            # response = openai.Completion.create(
-            #     engine="code-davinci-002",  # Use "codex" engine for code completions
-            #     prompt=prompt,
-            #     max_tokens=300  # Adjust the number of tokens in the completion
-            # )
-            # completion = response.choices[0].text.strip()
-            completion = "generate received"
+    global prompt
+    global completion 
 
     return render_template("index.html", prompt=prompt, completion=completion)
 
-@app.route("/generate", methods=["GET", "POST"])
+@app.route("/generate", methods=["POST"])
 def generate():
-    prompt = ""
-    completion = ""
-    json_data = str(request.get_json())
-    prompt = json_data
-    response = openai.Completion.create(
-        engine="code-davinci-002",  # Use "codex" engine for code completions
-        prompt=prompt,
-        max_tokens=300  # Adjust the number of tokens in the completion
-    )
-
-    completion = response.choices[0].text.strip()
-    return render_template("index.html", prompt=prompt, completion=completion)
-
+    global prompt
+    global completion 
+    json_data = str((request.get_json()))
+    print(json_data)
+    prompt = request.form.get("prompt")
+    print(prompt)
+    # Use the OpenAI API to generate a completion using "codex" engine
+    # response = openai.Completion.create(
+    #     engine="code-davinci-002",  # Use "codex" engine for code completions
+    #     prompt=prompt,
+    #     temperature=1.0,
+    #     max_tokens=256  # Adjust the number of tokens in the completion
+    # )
+    # completion = response.choices[0].text.strip()
+    completion="asdfasdf"
+    return jsonify({"completion": completion})
 
 
 if __name__ == "__main__":
