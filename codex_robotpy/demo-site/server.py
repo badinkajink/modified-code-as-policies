85d6529283953718f3f7b06a333f072331a3ca1f
badinkajink <williamxie00@gmail.com>
2023-08-18 00:24:32 -0400

    porting to pythonanywhere

--- codex_robotpy/demo-site/server.py
+++ codex_robotpy/demo-site/server.py
@@ -1,39 +1,32 @@
 from flask import Flask, render_template, request, jsonify
 import openai
 import os
-
+import simulated_romi_prompt as srp
 app = Flask(__name__)
 
 # Set your OpenAI API key here
 openai.api_key_path = "openai_api_key.txt"
 generate_clicked = False
-prompt = "# Enter your prompt here or upload a file"
+prompt = "# Enter a prompt or upload a file"
 completion = ""
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     global prompt
     global completion 
-
     return render_template("index.html", prompt=prompt, completion=completion)
 
 @app.route("/generate", methods=["POST"])
 def generate():
-    global prompt
-    global completion 
-    json_data = str((request.get_json()))
-    print(json_data)
-    prompt = request.form.get("prompt")
-    print(prompt)
+    prompt = str(request.get_json())
     # Use the OpenAI API to generate a completion using "codex" engine
-    # response = openai.Completion.create(
-    #     engine="code-davinci-002",  # Use "codex" engine for code completions
-    #     prompt=prompt,
-    #     temperature=1.0,
-    #     max_tokens=256  # Adjust the number of tokens in the completion
-    # )
-    # completion = response.choices[0].text.strip()
-    completion="asdfasdf"
+    response = openai.Completion.create(
+        engine="code-davinci-002",  # Use "codex" engine for code completions
+        prompt=srp.prompt + prompt,
+        temperature=1.0,
+        max_tokens=200  # Adjust the number of tokens in the completion
+    )
+    completion = response.choices[0].text.strip()
     return jsonify({"completion": completion})
 
 
